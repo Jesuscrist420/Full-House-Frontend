d69932cb85bfd22b69aab4ec3aa02ce63b82f171
Jesus Quinones <jequinonesc@unal.edu.co>
2024-04-05 07:53:26 -0500

    Fixing home settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/home.module.scss';
+import { lato, lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import HomeHeader from './ui/molecules/homeHeader/HomeHeader';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
+    <>
+      <HomeHeader />
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+          <p className={`${lato.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Bienvenido a FullHouse.</strong> Acabas de encontrar el App perfecta para optimizar{' '}
+            tu <strong>Restaurante!</strong>
           </p>
           <Link
             href="/login"
@@ -26,8 +24,22 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
-    </main>
+    </>
   );
 }
