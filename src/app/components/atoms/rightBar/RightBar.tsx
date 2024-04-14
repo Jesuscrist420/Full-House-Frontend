9e349b6ae70e495e21356ec754bd0a9928e9af59
Jesus Quinones <jequinonesc@unal.edu.co>
2024-04-14 13:14:05 -0500

    Creating RightBar template

---
+++ src/app/components/atoms/rightBar/RightBar.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useRef, type ReactNode } from 'react';
+import styles from './RightBar.module.scss';
+import { IoIosCloseCircle } from "react-icons/io";
+
+type rightBarProps = {
+    children?: ReactNode,
+    isOpen?: boolean,
+    setIsOpen: (val: boolean) => void,
+    title?: string,
+}
+
+const RightBar = ({ children, isOpen = false, setIsOpen, title = 'Title' }: rightBarProps) => {
+
+    const rightBarRef = useRef(null);
+
+    const handleClose = (): void => {
+        setIsOpen(false);
+    }
+
+    return (
+        <>
+            {isOpen ? (
+                <>
+                    <div className={styles.container} onClick={handleClose}>
+
+                    </div>
+                    <div className={styles.rightBar} ref={rightBarRef}>
+                        <div className = {styles.rightBarContainer}> 
+                            <header className={styles.rightBarHeader}>
+                                <h1 className={styles.title}>{title}</h1>
+                                <button onClick={handleClose}>
+                                    <IoIosCloseCircle size={20} />
+                                </button>
+                            </header>
+                            {children}
+                        </div>
+                    </div>
+                </>
+
+            ) : (
+                <></>
+            )}
+        </>
+    )
+}
+
+export default RightBar;
