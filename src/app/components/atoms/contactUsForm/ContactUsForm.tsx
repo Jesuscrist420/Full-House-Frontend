7986e7706d326a39bd4b84ea9f39bf7a4964cf32
Jesus Quinones <jequinonesc@unal.edu.co>
2024-04-15 08:58:21 -0500

    Cleaning info on contact us form

--- src/app/components/atoms/contactUsForm/ContactUsForm.tsx
+++ src/app/components/atoms/contactUsForm/ContactUsForm.tsx
@@ -1,8 +1,25 @@
 'use client'
+import { useState } from "react";
 import styles from "./ContactUsForm.module.scss";
-
+import Swal from "sweetalert2";
 
 const ContactUsForm = () => {
+    
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
 
     async function handleSubmit(event: any) {
         event.preventDefault();
@@ -22,16 +39,47 @@ const ContactUsForm = () => {
             body: json
         });
         const result = await response.json();
+        
         if (result.success) {
+            setName('');
+            setEmail('');
+            setMessage('');
+            void Toast.fire({
+                icon: 'success',
+                title: 'Correo Enviado'
+            })
             console.log(result);
         }
     }
 
     return (
         <form className={styles.formContainer} onSubmit={handleSubmit}>
-            <input required className={styles.input} type='text' name='name' placeholder='¿Cuál es tu nombre?' />
-            <input required className={styles.input} type='email' name='email' placeholder='¿Cuál es tu correo?' />
-            <textarea required className={styles.input} name='message' placeholder='Déjanos saber tu mensaje' />
+            <input 
+                required
+                className={styles.input}
+                type='text'
+                name='name'
+                value={name}
+                placeholder='¿Cuál es tu nombre?'
+                onChange={(e) => { setName(e.target.value); }}
+            />
+            <input 
+                required
+                className={styles.input}
+                type='email'
+                name='email'
+                value={email}
+                placeholder='¿Cuál es tu correo?'
+                onChange={(e) => { setEmail(e.target.value); }}
+            />
+            <textarea 
+                required
+                className={styles.input}
+                name='message'
+                value={message}
+                placeholder='Déjanos saber tu mensaje'
+                onChange={(e) => { setMessage(e.target.value); }}
+            />
             <button className={styles.submitButton} type="submit">Enviar</button>
         </form>
     )
