6070a48562176b8032d6da7f5292135c6d18d5d4
Jesus Quinones <jequinonesc@unal.edu.co>
2024-04-28 23:04:25 -0500

    Employees Grid, Employee Card, Employee Summary

--- src/app/dashboard/employees/page.tsx
+++ src/app/dashboard/employees/page.tsx
@@ -5,10 +5,24 @@ import RightBar from "@/app/components/atoms/rightBar/RightBar";
 import EmptyPage from "@/app/components/atoms/emptyPage/EmptyPage";
 import { useState } from "react";
 import CommonHeaderButton from "@/app/components/atoms/commonHeaderButton/CommonHeaderButton";
+import AddEmployeeForm from "@/app/components/molecules/addEmployeeForm/AddEmployeeForm";
+import EmployessGrid from "@/app/components/organisms/employeesGrid/EmployeesGrid";
 
 const Page = () => {
 
     const [addEmployeeIsOpen, setAddEmployeeIsOpen] = useState(false);
+
+    const [employeesList, setEmployeesList] = useState([
+        {
+            name: 'employeeName',
+            email: 'employeeEmail',
+            password: 'employeePassword',
+            role: 'Admin',
+            id: "653c0608195e0930f96230f7",
+            createdAt: "2023-11-26T18:49:41.082Z",
+            updatedAt: "2023-11-26T18:49:41.082Z"
+        },
+    ]);
 
     const handleAddEmployee = (): void => {
         setAddEmployeeIsOpen(true);
@@ -19,9 +33,10 @@ const Page = () => {
             <CommonHeader title='Empleados' >
                 <CommonHeaderButton text="Empleado" handleClick={handleAddEmployee} />
             </CommonHeader>
-            <EmptyPage handleClick={handleAddEmployee} emptyPage="Empleados" />
+            <EmptyPage handleClick={handleAddEmployee} emptyPage="Empleados" hidden={employeesList.length !== 0}/>
+            <EmployessGrid employeesList={employeesList} hidden={employeesList.length == 0}/>
             <RightBar isOpen={addEmployeeIsOpen} setIsOpen={setAddEmployeeIsOpen} title='Crear Empleado'>
-                {/* <AddEmployeeForm setAddEmployeeIsOpen={setAddEmployeeIsOpen} /> */}
+                <AddEmployeeForm setAddEmployeeIsOpen={setAddEmployeeIsOpen} employeesList={employeesList} />
             </RightBar>
         </>
     );
