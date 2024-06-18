6c9c2e85a5bfb8743ba2ab0569c393a8d4ef839b
xerojasga <xerojasga@unal.edu.co>
2024-06-18 18:08:45 -0500

    style(invoices/table.tsx): update table component styles for better readability and organization feat(addAccountForm): add a new form component for adding accounts to improve user experience and functionality feat(addAccountForm.module.scss): add styles for the new AddAccountForm component for consistent styling across the application
    feat(getAccounts.service.ts): add functions to interact with accounts API including get, add, update, delete, and close account
    feat(getAccounts.service.ts): remove redundant getAccounts service file as functions are now consolidated into one file

--- src/app/components/invoices/table.tsx
+++ src/app/components/invoices/table.tsx
@@ -1,7 +1,8 @@
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import styles from "../organisms/tablesAccordion/CategoriesAccordion.module.scss";
 import { FaPencilAlt } from 'react-icons/fa';
-import { MdDelete } from 'react-icons/md';
+import { MdClose, MdDelete } from 'react-icons/md';
+
 export default function Table({
   accounts,
   handleEdit,
@@ -46,49 +47,49 @@ export default function Table({
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-white shadow-md overflow-hidden">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Table ID</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Restaurant ID</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Comentario</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de apertura</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de cierre</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Usuario</th>
-                <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {accounts.map((account) => (
-                <tr key={account.id}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{account.id}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.table_id}</td>
-                  <td className={`px-6 py-4 whitespace-nowrap text-sm font-medium ${account.status === 'active' ? 'text-green-500' : 'text-red-500'}`}>{account.status}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.restaurant_id}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.comment}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatDateToLocal(account.opening_timestamp)}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatDateToLocal(account.closing_timestamp)}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatCurrency(account.total)}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.user_id}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <div className={styles.buttonsContainer}>
-                      <button className={styles.editButton} onClick={
-                        () => handleEdit(account)
-
-                      }><FaPencilAlt />Editar</button>
-                      <button className={styles.deleteButton} onClick={
-                        () => handleDelete(account)
-
-                      }><MdDelete />Eliminar</button>
-                    </div>
-                  </td>
+          <div className="max-h-96 overflow-y-auto"> {/* Added container for scrollable table */}
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Table ID</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Restaurant ID</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Comentario</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de apertura</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha de cierre</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Usuario</th>
+                  <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {accounts.map((account) => (
+                  <tr key={account.id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{account.id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.table_id}</td>
+                    <td className={`px-6 py-4 whitespace-nowrap text-sm font-medium ${account.status === 'active' ? 'text-green-500' : 'text-red-500'}`}>{account.status}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.restaurant_id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.comment}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatDateToLocal(account.opening_timestamp)}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatDateToLocal(account.closing_timestamp)}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{formatCurrency(account.total)}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{account.user_id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <div className={styles.buttonsContainer}>
+                        <button className={styles.editButton} onClick={() => handleEdit(account)}>
+                          <FaPencilAlt />Editar
+                        </button>
+                        <button className={styles.deleteButton} onClick={() => handleDelete(account)}>
+                          <MdClose />  Cerrar
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div> {/* End of scrollable container */}
         </div>
       </div>
     </div>
---
+++ src/services/accounts/getAccounts.service.ts
@@ -0,0 +1,105 @@
+export interface Account {
+    "id": number;
+    "closing_timestamp": string
+    "comment": string
+    "opening_timestamp": string
+    "restaurant_id": number
+    "status": string
+    "table_id": number
+    "total": number
+    "user_id": number
+
+}
+export interface CreateAccount {
+    "comment": string
+    "table_id": number
+    "total": number
+}
+export async function getAccounts(token: string | unknown): Promise<Account[] | undefined> {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    })
+
+    if (res.ok) {
+        const accountsList = await res.json();
+        return accountsList;
+    }
+
+    return undefined;
+}
+
+export async function addAccount(token: string | unknown, account: CreateAccount): Promise<any> {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(account)
+    })
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    return undefined;
+}
+
+export async function updateAccount(token: string | unknown, account: Account): Promise<any> {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts/${account.id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(account)
+    })
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    return undefined;
+}
+
+export async function deleteAccount(token: string | unknown, account: Account): Promise<any> {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts/${account.id}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    return undefined;
+}
+
+export async function closeAccount(token: string | unknown, account: Account): Promise<any> {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts/${account.id}/close`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(account)
+    })
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    return undefined;
+}
