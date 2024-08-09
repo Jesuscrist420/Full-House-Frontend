1ff6c0dcc3b7ca2972fb4606247261403be21ebd
Jesus Quinones <jequinonesc@unal.edu.co>
2024-08-09 08:33:16 -0500

    Dynamic rendering on Dashboard button depending on session

--- src/app/components/atoms/homeNavbar/HomeNavbar.tsx
+++ src/app/components/atoms/homeNavbar/HomeNavbar.tsx
@@ -8,6 +8,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import classnames from 'classnames';
 import { usePathname } from 'next/navigation';
 import { UrlObject } from "url";
+import { useSession } from "next-auth/react";
 
 type homeNavbarProps = {
     showDropdown?: boolean;
@@ -17,11 +18,14 @@ const HomeNavbar = ({ showDropdown }: homeNavbarProps) => {
 
     const pathname = usePathname();
     const navStyles = classnames(styles.nav, { [styles.active]: showDropdown });
+    const { data: session, status } = useSession();
+    const isLogged = session ? true : false;
 
     const links = [
         { name: 'Home', value: 'Home', href: '/' },
         { name: 'About Us', value: '¿Quiénes Somos?', href: '/aboutUs' },
         { name: 'Contact Us', value: 'Contáctanos', href: '/contactUs' },
+        { name: 'Dashboard', value: 'Dashboard', href: '/dashboard' },
         { name: 'Authentication', value: 'Empieza Ahora', href: '/authentication' },
     ];
 
@@ -30,6 +34,11 @@ const HomeNavbar = ({ showDropdown }: homeNavbarProps) => {
             <ul>
                 {links.map((link) => {
                     const selectedStyles = classnames({ [styles.selected]: pathname === link.href })
+                    if (link.name == 'Dashboard'){
+                        if (!isLogged){
+                            return;
+                        }
+                    }
                     return (
                         <li className={selectedStyles} key={link.name}><Link href={link.href}>{link.value}</Link></li>
                     )
