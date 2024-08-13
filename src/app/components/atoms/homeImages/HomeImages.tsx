64b190a772b4d332941571246b20eb4c929bfe77
Jesus Quinones <jequinonesc@unal.edu.co>
2024-08-13 04:57:35 -0500

    Fixing imgs

--- src/app/components/atoms/homeImages/HomeImages.tsx
+++ src/app/components/atoms/homeImages/HomeImages.tsx
@@ -4,14 +4,14 @@ const HomeImages = () => {
     return (
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
             <Image
-                src="/hero-desktop.png"
+                src="/full-house-desktop.JPG"
                 width={1000}
                 height={760}
                 className="hidden md:block"
                 alt="Screenshots of the dashboard project showing desktop version"
             />
             <Image
-                src="/hero-mobile.png"
+                src="/full-house-mobile.JPG"
                 width={560}
                 height={620}
                 className="block md:hidden"
